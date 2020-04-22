405aaa8d162ccb04f96ba1ae628b5f1938c85ce0
yfarook3 <60125652+yfarook3@users.noreply.github.com>
2020-04-22 03:05:47 -0500

    commit
    saving

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function init(){
+
+	document.getElementById("entrybutton").addEventListener("click", myAlert);
+	
+	fucntion myAlert(){
+				var inout = document.getElementById("entryinput").value
+				alert("Yaqub Farook: " + input); 
+	
+	
+	}
+	
+};
+ 
+	
+
+
+
+
+
+
+window.addEventListener('load', init);
